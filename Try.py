8ba84cb3c26a5f0f88eb4a3353cf93dc619e258e
Shaily4504 <sg2322@srmist.edu.in>
2025-08-16 13:25:43 +0530

    Streamlit Work

--- Try.py
+++ Try.py
@@ -4,7 +4,7 @@ import sentencepiece
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.chains.summarize import load_summarize_chain  
-from transformers import T5Tokenizer, T5ForConditionalGeneration, pipeline, AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM, AutoModel
 import torch
 import base64 
 import importlib
@@ -13,13 +13,14 @@ import sys
 
 #FILE LOADER AND PREPROCESSING
 #tokenizer = AutoTokenizer.from_pretrained("MBZUAI/LaMini-Flan-T5-248M")
+model="MBZUAI/LaMini-Flan-T5-248M"
 summarizer = pipeline("summarization", model="MBZUAI/LaMini-Flan-T5-248M")
 
 # If you also need tokenizer and model explicitly:
-tokenizer = AutoTokenizer.from_pretrained("MBZUAI/LaMini-Flan-T5-248M")
-model = AutoModelForSeq2SeqLM.from_pretrained("MBZUAI/LaMini-Flan-T5-248M")
+tokenizer = AutoTokenizer.from_pretrained(model)
+model = AutoModelForSeq2SeqLM.from_pretrained(model) 
 
-def file_preprocessing(file):
+def file_preprocessing(file):   
     loader = PyPDFLoader(file)
     pages = loader.load_and_split()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=50)
@@ -33,9 +34,9 @@ def file_preprocessing(file):
 #LLM PIPELINE
 def llm_pipeline(filepath):
     pipe_sum = pipeline(
-        'summerization',
+        'summarization',
         model = model,
-        tokernzier = tokenizer,
+        tokenizer = tokenizer,
         max_length = 500,
         min_length = 50,
     )
@@ -48,7 +49,7 @@ def llm_pipeline(filepath):
 @st.cache_data
 def display_PDF(file):
     with open(file, "rb") as f:
-        base64_pdf = base64.b64decode(f.read().decode('utf-8'))
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
     pdf_display = F'<ifram src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
@@ -59,7 +60,7 @@ def main():
     st.title('Document Classification using LLM')
     uploaded_file = st.file_uploader("Upload your PDF", type=['pdf'])
     if uploaded_file is not None:
-        if st.button("Summerization"):
+        if st.button("Summerize"):
             col1, col2 = st.columns(2)
             filepath = "Data_PDF/"+uploaded_file.name
             with open(filepath, 'wb') as temp_file:
@@ -68,7 +69,7 @@ def main():
                 st.info("Uploaded PDF file")
                 pdf_viewer = display_PDF(filepath)
             with col2:
-                st.info("Summerization is below")  
+                st.info("Summerization is below")    
                 summary = llm_pipeline(filepath)
                 st.success(summary)
 
